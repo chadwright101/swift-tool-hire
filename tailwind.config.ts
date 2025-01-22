c32fb1f8fe974eb4ea3d6740e9a72dd3b030d741
Chad Wright <justdeejays@gmail.com>
2025-01-22 15:29:20 +0200

    init commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,51 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      black: "#231F20",
+      white: "#FCFCFC",
+      lightGreen: "#A6CE39",
+      darkGreen: "#134B23",
+      linkBlue: "#0000FF",
+      transparent: "transparent",
+    },
+    fontSize: {
+      paragraph: [
+        "1rem",
+        {
+          letterSpacing: "0.02rem",
+          fontWeight: "300",
+        },
+      ],
+      subheading: [
+        "1.25rem",
+        {
+          letterSpacing: "0.025rem",
+          fontWeight: "600",
+        },
+      ],
+      heading: [
+        "2.25rem",
+        {
+          letterSpacing: "0.045rem",
+          fontWeight: "500",
+        },
+      ],
+    },
+    screens: {
+      phone: "425px",
+      tablet: "800px",
+      desktop: "1280px",
+    },
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      margin: {
+        15: "60px",
+      },
+      padding: {
+        15: "60px",
+      },
+      gap: {
+        15: "60px",
       },
     },
   },
